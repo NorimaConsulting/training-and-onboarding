6624305b7e39b261e7499643f23dea0e67f9715f
Signe Bone <signe.bone@norimaconsulting.com>
2022-01-20 11:28:58 -0700

    added rough version fo recipe page

--- webapp/src/pages/Recipe/Recipe.tsx
+++ webapp/src/pages/Recipe/Recipe.tsx
@@ -1,10 +1,26 @@
 import React from 'react';
+import { Panel } from 'primereact/panel';
+import { Splitter, SplitterPanel } from 'primereact/splitter';
 import './Recipe.css';
 
 export default function Recipe() {
   return (
     <div className="recipe">
-      <h1>I am a single glorious recipe!</h1>
+      <h1>Recipe Title: </h1>
+      <h2>Posted By: </h2>
+      <p>Prep Time: </p>
+      <img src="" alt="" />
+      <Panel header="Description">
+        <p>A recipe Description: Something delicous</p>
+      </Panel>
+      <Splitter style={{ height: '300px' }} className="mb-5">
+        <SplitterPanel className="flex align-items-center justify-content-center">
+          Ingredients:
+        </SplitterPanel>
+        <SplitterPanel className="flex align-items-center justify-content-center">
+          Instructions:
+        </SplitterPanel>
+      </Splitter>
     </div>
   );
 }
