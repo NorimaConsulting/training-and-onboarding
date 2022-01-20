a9f7204dcf15e4127143833b380c15d97c2de167
Arsh Singh Buttar <arshpreet.buttar@Norimaconsulting.com>
2022-01-20 12:37:21 -0600

    added file to .prettierignore

--- webapp/codegen.js
+++ webapp/codegen.js
@@ -1,27 +1,23 @@
 require('dotenv').config({path:'../.env'});
 
-module.exports = {
-  schema: [
-    {
-      [`${process.env.HASURA_GRAPHQL_ENDPOINT}/v1/graphql`]: {
-        headers: {
-          'x-hasura-admin-secret': process.env.HASURA_GRAPHQL_ADMIN_SECRET
-        }
+export const schema = [
+  {
+    [`${process.env.HASURA_GRAPHQL_ENDPOINT}/v1/graphql`]: {
+      headers: {
+        'x-hasura-admin-secret': process.env.HASURA_GRAPHQL_ADMIN_SECRET
       }
     }
-  ],
-  // TODO: Figure out why it works when commented out
-  // documents: ['src/**/*.graphql'],
-  generates: {
-    'src/resources/config/generated/resourceApi/index.ts': {
-      plugins: [
-        'typescript',
-        'typescript-operations',
-        'typescript-urql',
-      ],
-    },
-    'src/resources/config/generated/graphql.schema.json': {
-      plugins: ['introspection'],
-    },
+  }
+];
+export const generates = {
+  'src/resources/config/generated/resourceApi/index.ts': {
+    plugins: [
+      'typescript',
+      'typescript-operations',
+      'typescript-urql',
+    ],
+  },
+  'src/resources/config/generated/graphql.schema.json': {
+    plugins: ['introspection'],
   },
 };
