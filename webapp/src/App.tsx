e5ccb9e4a191de7e524ab4e4849ac74532c1f5b1
Signe Bone <signe.bone@norimaconsulting.com>
2022-01-20 17:05:09 -0700

    resolved most ESlint issues expect one Auth0 one in App.tsx

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -7,7 +7,7 @@ import CreatedBy from './pages/CreatedBy';
 import HomePage from './pages/HomePage';
 import Recipe from './pages/Recipe';
 import RecipeList from './pages/RecipeList';
-import UserProfile from './pages/UserProfile/UserProfile';
+import UserProfile from './pages/UserProfile';
 import ButtonArsh from './components/atoms/ButtonArsh';
 import UsersQuery from './components/atoms/UsersQuery';
 import { createClient, Provider } from 'urql';
@@ -53,46 +53,45 @@ function App() {
       redirectUri={window.location.origin}
     >
       <div className="App">
-      <Provider value={client}>
-        <MainScreen />
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="home" element={<HomePage />} />
-          <Route path="recipe/:ID" element={<Recipe />} />
-          <Route path="recipe-list" element={<RecipeList />} />
-          <Route path="profile" element={<UserProfile />}>
-            <Route path=":userName" element={<UserProfile />} />
-          </Route>
+        <Provider value={client}>
+          <MainScreen />
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="home" element={<HomePage />} />
+            <Route path="recipe/:ID" element={<Recipe />} />
+            <Route path="recipe-list" element={<RecipeList />} />
+            <Route path="profile" element={<UserProfile />}>
+              <Route path=":userName" element={<UserProfile />} />
+            </Route>
 
-          {/* Leave this route, but don't necessarily display it once you have real features. */}
-          <Route path="/created_by" element={<CreatedBy />}>
-            {/* Using the same element since it's handling both. Normally you'd probably have a separate one */}
-            <Route path=":displayName" element={<SoundEffectTWO />} />
+            {/* Leave this route, but don't necessarily display it once you have real features. */}
+            <Route path="/created_by" element={<CreatedBy />}>
+              {/* Using the same element since it's handling both. Normally you'd probably have a separate one */}
+              <Route path=":displayName" element={<SoundEffectTWO />} />
+              <Route
+                path="/created_by/yellow-power-ranger/:displayName"
+                element={<SoundEffect />}
+              ></Route>
+            </Route>
             <Route
-              path="/created_by/yellow-power-ranger/:displayName"
-             
-              element={<SoundEffect />}
-            ></Route>
-          </Route>
-          <Route
-            path="*"
-            element={
-              <div>
-                <p>
-                  Sorry the page you&apos;re looking for isn&apos;t at this
-                  address 😥
-                </p>
-                <p>please double check the url!</p>
-              </div>
-            }
-          />
-          <Route path="/blackRanger" element={<CreatedBy />} />
-          <Route path="/newbie" element={<ButtonArsh />} />
-          {/* Remove this route when not needed for testing query purposes */}
-          <Route path="/testQuery" element={<UsersQuery />} />
-        </Routes>
-      </Provider>
-    </div>
+              path="*"
+              element={
+                <div>
+                  <p>
+                    Sorry the page you&apos;re looking for isn&apos;t at this
+                    address 😥
+                  </p>
+                  <p>please double check the url!</p>
+                </div>
+              }
+            />
+            <Route path="/blackRanger" element={<CreatedBy />} />
+            <Route path="/newbie" element={<ButtonArsh />} />
+            {/* Remove this route when not needed for testing query purposes */}
+            <Route path="/testQuery" element={<UsersQuery />} />
+          </Routes>
+        </Provider>
+      </div>
     </Auth0Provider>
   );
 }
