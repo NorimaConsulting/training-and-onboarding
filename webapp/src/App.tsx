c702543c65304b5b28e336cf6b9d2a0cc341e070
Arsh Singh Buttar <arshpreet.buttar@Norimaconsulting.com>
2022-01-20 20:38:34 -0600

    fixed linter check and added file to prettierignore

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -93,7 +93,7 @@ function App() {
             <Route path="/testQuery" element={<UsersQuery />} />
           </Routes>
         </Provider>
-        </div>
+      </div>
     </Auth0Provider>
   );
 }
